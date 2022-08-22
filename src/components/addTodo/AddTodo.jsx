3c2d1ee98de44ef16f6aaa92108c3fade37cae49
wilkenson <tcbwilkenson@gmail.com>
2022-08-21 21:59:49 -0400

    add shodow-box and border

--- src/components/addTodo/AddTodo.jsx
+++ src/components/addTodo/AddTodo.jsx
@@ -10,7 +10,7 @@ const AddTodo = () => {
     <>
       <form className="container d-flex justify-content-center form-container">
         <div className="addTodo-container todo-form-container">
-          <div>
+          <div className="box-shodow">
             <input
               className="input-todo text-white todo-bg"
               type="text"
@@ -18,39 +18,30 @@ const AddTodo = () => {
             />
             <input className="input-btn" type="radio" />
           </div>
-          <div className="todo-bg mt-3 list-todo text-white">
+          <div className="todo-bg mt-4 list-todo text-white box-shodow">
             <ul className="list-unstyled pl-2">
-              <li>
+              <li className="border-bottom border-dark">
                 <input type="radio" /> a todo is added
               </li>
-              <li>
-                <input type="radio" /> a todo is added{" "}
+              <li className="border-bottom border-dark">
+                <input type="radio" /> a todo is added
               </li>
-              <li>
-                <input type="radio" /> a todo is added{" "}
+              <li className="border-bottom border-dark">
+                <input type="radio" /> a todo is added
               </li>
-              <li>
-                <input type="radio" /> a todo is added{" "}
-              </li>
-              <li>
-                <input type="radio" /> a todo is added{" "}
-              </li>
-              <li>
-                <input type="radio" /> a todo is added{" "}
-              </li>
-              <li>
-                <input type="radio" /> a todo is added{" "}
+              <li className="border-bottom border-dark">
+                <input type="radio" /> a todo is added
               </li>
             </ul>
             <div className="container">
-              <div className="row pt-3 action pb-2">
-                <div className=" pl-3 pr-5">Items left</div>
+              <div className="row action pb-2">
+                <div className=" pl-3 pr-5 btn text-white">Items left</div>
                 <div className="d-flex">
-                  <div className="pl-3 pr-4">All</div>
-                  <div className="pr-4">Active </div>
-                  <div className="pr-4">Completed</div>
+                  <div className="pl-3 pr-4 btn text-white">All</div>
+                  <div className="pr-4 btn text-white">Active </div>
+                  <div className="pr-4 btn text-white">Completed</div>
                 </div>
-                <div className="col">Clear Completed</div>
+                <div className="col btn text-white">Clear Completed</div>
               </div>
             </div>
           </div>
